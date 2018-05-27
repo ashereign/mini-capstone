5b6d7ef2b96e2e2ebf64d333909a2ac2a6e0154d
asher <ashermillhollin@gmail.com>
2018-05-27 14:17:41 -0700

    added first 3 products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,9 @@
 class Api::ProductsController < ApplicationController
+
+  def product_action
+    @products = Product.all 
+    render 'products_view.json.jbuilder'
+  end
+
+
 end
---
+++ app/views/api/products/products_view.json.jbuilder
@@ -0,0 +1,2 @@
+# json.product_test_key "hello"
+
